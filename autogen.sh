f3eca3e6d0aa7a0875911391c04f1b60565b5eed
Geoffroy Vallee <valleegr@ornl.gov>
2017-02-06 23:15:00 -0500

    Switch to autotools so we can specify a prefix and benefit from standard make option out-of-the-box.

---
+++ autogen.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# Copyright (c) 2017        UT-Battelle, LLC
+#                           All rights reserved.
+#
+# See COPYING in top-level directory.
+# 
+# Additional copyrights may follow
+# 
+# $HEADER$
+#
+
+autoreconf -ifv
